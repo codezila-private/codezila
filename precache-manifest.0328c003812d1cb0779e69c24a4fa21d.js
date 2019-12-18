fb00248465113ed67cfa741908d387fce1d7dff1
AmanSingour <amansingour15@gmail.com>
2019-12-19 03:00:36 +0530

    Second Launch

---
+++ precache-manifest.0328c003812d1cb0779e69c24a4fa21d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33886087ca9422026f3764468eead3d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2118ba8cf0e9ec4e5c47",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "23fa4362a876e8a63101",
+    "url": "/static/css/main.9b70b51e.chunk.css"
+  },
+  {
+    "revision": "2118ba8cf0e9ec4e5c47",
+    "url": "/static/js/2.34de133e.chunk.js"
+  },
+  {
+    "revision": "b15a737ad2cfd16685fd27e8e85315d2",
+    "url": "/static/js/2.34de133e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23fa4362a876e8a63101",
+    "url": "/static/js/main.6316d764.chunk.js"
+  },
+  {
+    "revision": "3f279c2b9de7f6ba4e71",
+    "url": "/static/js/runtime-main.ec74131f.js"
+  },
+  {
+    "revision": "638750de1f69fe98f0ac818c8fdccb80",
+    "url": "/static/media/czboy.638750de.jpg"
+  },
+  {
+    "revision": "bf3a546cc9d8fe9c621975216ceeb7cc",
+    "url": "/static/media/footerbac.bf3a546c.svg"
+  },
+  {
+    "revision": "8f84a42a3b66ede07bedbb7b5b796271",
+    "url": "/static/media/headerbac.8f84a42a.svg"
+  },
+  {
+    "revision": "5ff74da4414cf29dc53b4fff8cbab58d",
+    "url": "/static/media/instructor.5ff74da4.png"
+  },
+  {
+    "revision": "e4a4283980d4f53e8623d6627a893f84",
+    "url": "/static/media/logo_tagline@288x.e4a42839.svg"
+  }
+]);
