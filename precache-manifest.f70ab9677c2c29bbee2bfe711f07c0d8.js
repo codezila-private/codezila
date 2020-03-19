e60a46c2d3abc12b87686762bc6472946929d1a5
AmanSingour <amansingour15@gmail.com>
2020-03-20 02:55:36 +0530

    Website Update

---
+++ precache-manifest.f70ab9677c2c29bbee2bfe711f07c0d8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f777d7e029d249dc8ee57e2b27b1ee8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfa34a890d9c5998602e",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "fa19fa64b0cd3ee80669",
+    "url": "/static/css/main.ad003cff.chunk.css"
+  },
+  {
+    "revision": "bfa34a890d9c5998602e",
+    "url": "/static/js/2.5d489a9b.chunk.js"
+  },
+  {
+    "revision": "fb6d39d6592d2b82fae291021910122b",
+    "url": "/static/js/2.5d489a9b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa19fa64b0cd3ee80669",
+    "url": "/static/js/main.6660c1d9.chunk.js"
+  },
+  {
+    "revision": "3f279c2b9de7f6ba4e71",
+    "url": "/static/js/runtime-main.ec74131f.js"
+  },
+  {
+    "revision": "bf3a546cc9d8fe9c621975216ceeb7cc",
+    "url": "/static/media/footerbac.bf3a546c.svg"
+  },
+  {
+    "revision": "8f84a42a3b66ede07bedbb7b5b796271",
+    "url": "/static/media/headerbac.8f84a42a.svg"
+  },
+  {
+    "revision": "5ff74da4414cf29dc53b4fff8cbab58d",
+    "url": "/static/media/instructor.5ff74da4.png"
+  },
+  {
+    "revision": "e4a4283980d4f53e8623d6627a893f84",
+    "url": "/static/media/logo_tagline.e4a42839.svg"
+  }
+]);
