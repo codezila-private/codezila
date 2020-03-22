9f93a6344008ab568e5bee4d19e1e2c5b4d20758
AmanSingour <amansingour15@gmail.com>
2020-03-22 15:43:04 +0530

    Update

---
+++ precache-manifest.47c5a3fbb88b1a9815da485f26f5339e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09133be9ac79a1761cf4f5eefa956875",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "634f63016cab70f3ff9b",
+    "url": "/home/static/css/2.65b5a4e9.chunk.css"
+  },
+  {
+    "revision": "7f272e323cea1bd0d65b",
+    "url": "/home/static/css/main.44eedaab.chunk.css"
+  },
+  {
+    "revision": "634f63016cab70f3ff9b",
+    "url": "/home/static/js/2.7768b973.chunk.js"
+  },
+  {
+    "revision": "f5beace4d4bce37985ab18dbe3be3376",
+    "url": "/home/static/js/2.7768b973.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f272e323cea1bd0d65b",
+    "url": "/home/static/js/main.1d556554.chunk.js"
+  },
+  {
+    "revision": "dc1a5e062ba262b83d0e",
+    "url": "/home/static/js/runtime-main.b7983f7f.js"
+  },
+  {
+    "revision": "bf3a546cc9d8fe9c621975216ceeb7cc",
+    "url": "/home/static/media/footerbac.bf3a546c.svg"
+  },
+  {
+    "revision": "8f84a42a3b66ede07bedbb7b5b796271",
+    "url": "/home/static/media/headerbac.8f84a42a.svg"
+  },
+  {
+    "revision": "5ff74da4414cf29dc53b4fff8cbab58d",
+    "url": "/home/static/media/instructor.5ff74da4.png"
+  },
+  {
+    "revision": "e4a4283980d4f53e8623d6627a893f84",
+    "url": "/home/static/media/logo_tagline.e4a42839.svg"
+  }
+]);
